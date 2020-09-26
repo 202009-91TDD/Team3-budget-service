1c1efa8e79aeaba5b2a93d0009563bda603d22ef
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 21:23:58 +0800

    use the same overlapping_end and overlapping_start for each if statement

--- budgetservice.py
+++ budgetservice.py
@@ -25,15 +25,15 @@ class BudgetService(object):
             daily_amount = budget.amount / days_of_budget
 
             if is_start_budget and is_end_budget:
-                overlapping_end = end
-                overlapping_start = start
+                overlapping_end = end if end < budget_last_day else budget_last_day
+                overlapping_start = start if start > budget_first_day else budget_first_day
 
             elif is_start_budget:
-                overlapping_end = budget_last_day
+                overlapping_end = end if end < budget_last_day else budget_last_day
                 overlapping_start = start if start > budget_first_day else budget_first_day
 
             elif begin_month < budget.yearMonth < end_month:
-                overlapping_end = budget_last_day
+                overlapping_end = end if end < budget_last_day else budget_last_day
                 overlapping_start = start if start > budget_first_day else budget_first_day
 
             elif is_end_budget:
