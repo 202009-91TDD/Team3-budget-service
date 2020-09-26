7fd9a6d7e0fd97e5bdb114514934d1c650f4aa3c
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 21:34:20 +0800

    inline variable: budget_last_day

--- budgetservice.py
+++ budgetservice.py
@@ -5,14 +5,12 @@ class BudgetService(object):
 
         total_amount = 0
         for budget in self.get_budgets():
-            budget_last_day = budget.last_day()
-
             daily_amount = budget.amount / budget.days()
 
-            if end < budget.first_day() or start > budget_last_day:
+            if end < budget.first_day() or start > budget.last_day():
                 break
 
-            overlapping_end = end if end < budget_last_day else budget_last_day
+            overlapping_end = end if end < budget.last_day() else budget.last_day()
             overlapping_start = start if start > budget.first_day() else budget.first_day()
             overlapping_days = ((overlapping_end - overlapping_start).days + 1)
             total_amount += round(daily_amount * overlapping_days, 2)
