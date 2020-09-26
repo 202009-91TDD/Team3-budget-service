672c0a6bebaa6c82de85a77ab007c2d0a8946b2f
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 21:30:48 +0800

    inline variable: budget.first_day()

--- budgetservice.py
+++ budgetservice.py
@@ -10,17 +10,16 @@ class BudgetService(object):
 
         total_amount = 0
         for budget in self.get_budgets():
-            budget_first_day = budget.first_day()
-            days_of_budget = calendar.monthrange(budget_first_day.year, budget_first_day.month)[1]
-            budget_last_day = budget_first_day.replace(day=days_of_budget)
+            days_of_budget = calendar.monthrange(budget.first_day().year, budget.first_day().month)[1]
+            budget_last_day = budget.first_day().replace(day=days_of_budget)
 
             daily_amount = budget.amount / days_of_budget
 
-            if end < budget_first_day or start > budget_last_day:
+            if end < budget.first_day() or start > budget_last_day:
                 break
 
             overlapping_end = end if end < budget_last_day else budget_last_day
-            overlapping_start = start if start > budget_first_day else budget_first_day
+            overlapping_start = start if start > budget.first_day() else budget.first_day()
             overlapping_days = ((overlapping_end - overlapping_start).days + 1)
             total_amount += round(daily_amount * overlapping_days, 2)
 
