ec3a831c0e3647cb8312e9188a9d31d94f178289
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 21:44:22 +0800

    move without overlapping if condition to period.overlapping_days()

--- budgetservice.py
+++ budgetservice.py
@@ -4,6 +4,8 @@ class Period(object):
         self.end = end
 
     def overlapping_days(self, budget):
+        if self.end < budget.first_day() or self.start > budget.last_day():
+            return 0
         overlapping_end = self.end if self.end < budget.last_day() else budget.last_day()
         overlapping_start = self.start if self.start > budget.first_day() else budget.first_day()
         overlapping_days = ((overlapping_end - overlapping_start).days + 1)
@@ -17,12 +19,8 @@ class BudgetService(object):
 
         total_amount = 0
         for budget in self.get_budgets():
-            if end < budget.first_day() or start > budget.last_day():
-                break
-
             period = Period(start, end)
-            overlapping_days = period.overlapping_days(budget)
-            total_amount += round(budget.daily_amount() * overlapping_days, 2)
+            total_amount += round(budget.daily_amount() * period.overlapping_days(budget), 2)
 
         return total_amount
 
