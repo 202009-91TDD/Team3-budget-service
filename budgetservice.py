550d3e7319748a7c89303cac9c6532ac2a98a6a7
Joey Chen <jay.hatelove@gmail.com>
2020-09-26 21:28:32 +0800

    extract method: first_day() of budget

--- budgetservice.py
+++ budgetservice.py
@@ -15,12 +15,9 @@ class BudgetService(object):
 
         total_amount = 0
         for budget in budgetsList:
-            budget_first_day = datetime.strptime(budget.yearMonth, "%Y%m")
+            budget_first_day = self.first_day(budget)
             days_of_budget = calendar.monthrange(budget_first_day.year, budget_first_day.month)[1]
             budget_last_day = budget_first_day.replace(day=days_of_budget)
-
-            is_start_budget = budget.yearMonth == start.strftime("%Y%m")
-            is_end_budget = budget.yearMonth == end.strftime("%Y%m")
 
             daily_amount = budget.amount / days_of_budget
 
@@ -29,28 +26,15 @@ class BudgetService(object):
 
             overlapping_end = end if end < budget_last_day else budget_last_day
             overlapping_start = start if start > budget_first_day else budget_first_day
-            # if is_start_budget and is_end_budget:
-            #     overlapping_end = end if end < budget_last_day else budget_last_day
-            #     overlapping_start = start if start > budget_first_day else budget_first_day
-            #
-            # elif is_start_budget:
-            #     overlapping_end = end if end < budget_last_day else budget_last_day
-            #     overlapping_start = start if start > budget_first_day else budget_first_day
-            #
-            # elif begin_month < budget.yearMonth < end_month:
-            #     overlapping_end = end if end < budget_last_day else budget_last_day
-            #     overlapping_start = start if start > budget_first_day else budget_first_day
-            #
-            # elif is_end_budget:
-            #     overlapping_end = end if end < budget_last_day else budget_last_day
-            #     overlapping_start = start if start > budget_first_day else budget_first_day
-            #
-            # else:
-            #     break
             overlapping_days = ((overlapping_end - overlapping_start).days + 1)
             total_amount += round(daily_amount * overlapping_days, 2)
 
         return total_amount
+
+    @staticmethod
+    def first_day(budget):
+        budget_first_day = datetime.strptime(budget.yearMonth, "%Y%m")
+        return budget_first_day
 
     def get_budgets(self):
         pass
